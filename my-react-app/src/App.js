54112cd89776c8b22ff277a25094e3a1aa4263c8
Marty Walters <martywalters@yahoo.com>
2025-09-18 20:50:32 +0000

    final

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProductDetails from "./examples/ProductDetails";
 import ProductList from "./examples/ProductList";
+import DynamicGrid from "./examples/DynamicGrid";
+import GlobalStyles from "./examples/GlobalStyles";
 
 const App = () => {
+
+  const items = ['item1', 'item2', 'item3', 'item4'];
   return (
     <div>
-      <ProductList />
+      <GlobalStyles />
+      <h1>Global Styles Example</h1>
+      <DynamicGrid items={items} />
     </div>
   );
 };
